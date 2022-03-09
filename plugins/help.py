79b9b8ab72739591a251bbc364cf557566c1473a
Zeta <81767970+zeta-thisara@users.noreply.github.com>
2022-03-09 20:38:13 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,8 +4,8 @@ import config
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
 	alpha2 = InlineKeyboardMarkup([
-		[InlineKeyboardButton("ʀᴇᴘᴏʀᴛ ʙᴜɢs 👾", url="https://t.me/AlphaX_SUPPORT")]
+		[InlineKeyboardButton("🆁🅴🅿︎🅾︎🆁🆃 🅴🆁🆁🅾︎🆁🆂 👾", url="https://t.me/sthisara_favorites_disscussion")]
                 ])
 
 	help_image = config.HPIC
-	await message.reply_photo(help_image,  caption="**💡 HELP Menu...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\•Thank You For Using Eplison Bot ☺️⚡️`\n\n",reply_markup=alpha2)
+	await message.reply_photo(help_image,  caption="**⚡️𝗘𝗽𝗹𝗶𝘀𝗼𝗻 𝗛𝗲𝗹𝗽 𝗠𝗲𝗻𝘂 ⚡️**\n \n__• 𝙅𝙪𝙨𝙩 𝙎𝙚𝙣𝙙 𝙔𝙤𝙪𝙧 𝙔𝙩 𝙑𝙞𝙙𝙚𝙤 𝙡𝙞𝙣𝙠__\n \n__• 𝘼𝙣𝙙 𝙄 𝙬𝙞𝙡𝙡 𝙜𝙞𝙫𝙚 𝙈𝙚𝙩𝙝𝙤𝙙 𝙡𝙞𝙨𝙩 𝙩𝙤 𝙎𝙚𝙡𝙚𝙘𝙩 𝙔𝙤𝙪𝙧 𝘾𝙝𝙤𝙞𝙘𝙚  💃🏼__\n \n ⚡️ 🄳 ᵉ ᵛ ᵃ ˡ ᵒ ᵖ ᵉ ᵈ   ᵇ ʸ  @sthisara ⚡️\`\n\n",reply_markup=alpha2)
